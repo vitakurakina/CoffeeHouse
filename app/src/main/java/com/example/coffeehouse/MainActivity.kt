b3f89054e578575066469a9853366e7b2b3cfd3f
vitakurakina <talina.krina@gmail.com>
2025-10-31 15:07:49 +0300

    Light and Dark themes

--- app/src/main/java/com/example/coffeehouse/MainActivity.kt
+++ app/src/main/java/com/example/coffeehouse/MainActivity.kt
@@ -10,22 +10,22 @@ class MainActivity : AppCompatActivity() {
     private val menuFragment = MenuFragment()
     private val accountFragment = AccountFragment()
     private val offersFragment = OffersFragment()
+    private val mapFragment = MapFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav_container)
 
-        // Установим Home по умолчанию
         bottomNavigation.selectedItemId = R.id.nav_home
 
-        // Показываем HomeFragment по умолчанию
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, homeFragment)
             .commit()
 
         bottomNavigation.setOnItemSelectedListener { item ->
             val selectedFragment = when(item.itemId) {
+                R.id.nav_map -> mapFragment
                 R.id.nav_home -> homeFragment
                 R.id.nav_menu -> menuFragment
                 R.id.nav_account -> accountFragment
---
+++ app/src/main/java/com/example/coffeehouse/MapFragment.kt
@@ -0,0 +1,9 @@
+package com.example.coffeehouse
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MapFragment : Fragment(R.layout.fragment_map)
