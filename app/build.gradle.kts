1ffd52fed7044644cf1551b0f043bc91db9955c0
vitakurakina <talina.krina@gmail.com>
2025-11-29 13:35:25 +0300

    navigation bar fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.coffeehouse"
-        minSdk = 24
+        minSdk = 27
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
